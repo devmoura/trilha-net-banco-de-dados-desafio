d5745e7580b6c48412b3e5a9fcba96c989b38e24
Tarcísio Moura <tatamoura2020@gmail.com>
2024-05-28 16:33:50 -0300

    Novo Arquivo

---
+++ SQL Query Desafio.sql
@@ -0,0 +1,59 @@
+
+-- 1-
+SELECT Nome, Ano FROM Filmes;
+
+
+-- 2-
+SELECT Nome, Ano, Duracao FROM Filmes 
+ORDER BY Ano;
+
+
+-- 3-
+SELECT Nome, Ano, Duracao FROM Filmes WHERE Nome = 'De Volta para o futuro';
+
+
+-- 4-
+SELECT Nome, Ano, Duracao FROM Filmes WHERE ANO = 1997;
+
+
+-- 5-
+SELECT Nome, Ano, Duracao FROM Filmes WHERE ANO > 2000;
+
+
+-- 6-
+SELECT Nome, Ano, Duracao FROM Filmes WHERE Duracao > 100 AND Duracao < 150
+ORDER BY Duracao;
+-- 7- 
+SELECT Ano, COUNT(*) AS Quantidade FROM Filmes
+GROUP BY Ano
+ORDER BY Quantidade DESC;
+
+
+-- 8-
+SELECT PrimeiroNome, UltimoNome FROM Atores;
+
+
+-- 9-
+SELECT PrimeiroNome, UltimoNome FROM Atores WHERE Genero = 'M'
+ORDER BY PrimeiroNome;
+-- 10-
+SELECT PrimeiroNome, UltimoNome FROM Atores WHERE Genero = 'F'
+ORDER BY PrimeiroNome;
+
+
+-- 11-
+SELECT Nome FROM Filmes WHERE FilmesGenero = 'Mistério';
+
+
+-- 12-
+SELECT 
+    Filmes.Nome,
+    Atores.PrimeiroNome,
+    Atores.UltimoNome,
+    ElencoFilme.Papel
+FROM 
+    Atores
+INNER JOIN 
+    ElencoFilme ON Atores.Id = ElencoFilme.IdAtor
+INNER JOIN
+    Filmes ON ElencoFilme.IdFilme = Filmes.Id;
